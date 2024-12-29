706ef38032be9cba7131b0b7a6584a2462331df6
Theng Wei Seng <t.w.seng96@gmail.com>
2024-12-29 22:30:17 +0800

    Retry setting in github.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // lightweight but no dynamic routes & api support
+  output: "exports", // lightweight but no dynamic routes & api support
   // output: "standalone", // bulkier but supports dynamic routes & api
 };
 
